bc6f18cdbf0a9d056a1044b3e1ae1103214bac4e
Vishal Shrivastava <s96viky@gmail.com>
2024-02-28 20:43:02 +0530

    responsive header and footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,20 +3,22 @@ import React from 'react'
 
 function Footer() {
     return (
-        <footer className='flex h-9 border-border border-t justify-between px-3'>
-            <div className='flex text-primary-foreground text-sm'>
-                <p
-                    className='border-r h-full border-[#1e2d3d] px-3 flex items-center'>
-                    find me in:
-                </p>
+        <footer className='flex flex-none h-9 border-border border-t text-primary-foreground text-sm w-full'>
+            <p
+                className='border-r h-full border-[#1e2d3d] px-3 flex items-center flex-1 md:flex-none'>
+                find me in:
+            </p>
+            <div className='flex flex-1 md:justify-between'>
+            <div className='flex '>
                 <a href="https://www.facebook.com/vishal.shrivastav.374" target='_blank' className='border-r h-full border-border px-4 flex items-center hover:bg-border'><FacebookIcon size={18} /></a>
                 <a href="https://www.instagram.com/vishaalovercome" target='_blank' className='border-r h-full border-border px-4 flex items-center hover:bg-border'><Instagram size={18} /></a>
                 <a href="https://twitter.com/s96viky" target='_blank' className='border-r h-full border-border px-4 flex items-center hover:bg-border'><Twitter size={18} /></a>
             </div>
-            <a  href="https://github.com/developervishal" target='_blank' className='flex items-center border-border border-l text-primary-foreground text-sm'>
-                <p className='px-3'>@s96vishal</p>
+            <a href="https://github.com/developervishal" target='_blank' className='border-l gap-2 h-full border-border px-4 flex items-center hover:bg-border'>
+                <p className='hidden md:block'>@s96vishal</p>
                 <Github size={18} />
             </a>
+            </div>
         </footer>
     )
 }
--- components/header.tsx
+++ components/header.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import clsx from 'clsx'
+import { Menu } from 'lucide-react'
 const headerLinks = [
     {
         name: 'introduction',
@@ -25,28 +26,31 @@ function Header() {
     const pathName = usePathname()
     console.log(pathName)
     return (
-        <header className='h-11 border-b  border-border flex  justify-between'>
-            <div className='flex text-sm text-primary-foreground '>
+        <header className='h-11 border-b border-border flex text-primary-foreground'>
+            <div className='w-full flex items-center justify-between md:border-r border-border md:w-56 gap-2 px-2'>
+                <Menu className='md:hidden' />
                 <Link
                     href="/"
-                    className='border-r border-border flex items-center w-56 px-3'>
+                    className='md:px-4 text-sm order-[-1]'>
                     vishal-shrivastava
                 </Link>
-                {headerLinks.map(link => (
-                    <Link
-                        key={link.name}
-                        className={clsx(
-                            'text-primary-foreground border-r h-full border-border px-4 flex items-center hover:bg-border hover:text-[#ffffff] text-sm transition-all',
-                            { 'border-b-warning border-b-2': pathName === link.to }
-                        )}
-                        href={link.to}>{link.displayName}</Link>
-                ))}
             </div>
-            <div className='flex items-center text-sm'>
+            <div className='md:flex hidden justify-between flex-grow'>
+                <div className='flex'>
+                    {headerLinks.map(link => (
+                        <Link
+                            key={link.name}
+                            className={clsx(
+                                'text-primary-foreground border-r h-full border-border px-4 flex items-center hover:bg-border hover:text-[#ffffff] text-sm transition-all',
+                                { 'border-b-warning border-b-2': pathName === link.to }
+                            )}
+                            href={link.to}>{link.displayName}</Link>
+                    ))}
+                </div>
                 <Link
                     className={clsx(
                         'border-l h-full border-border flex items-center text-primary-foreground px-2 hover:bg-border',
-                        { 'border-b-warning border-b-2': pathName === '/contact'}
+                        { 'border-b-warning border-b-2': pathName === '/contact' }
                     )}
                     href="/contact"
                 >_contact-me</Link>
